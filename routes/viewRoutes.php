5beb2a5ed7a233fcf2fe6cfc6e47091527f0accc
Mark Cueva <markcueva6@gmail.com>
2025-06-24 22:17:25 +0800

    Added Accounting Page for Staff

--- routes/viewRoutes.php
+++ routes/viewRoutes.php
@@ -136,3 +136,28 @@ Route::get('/changepassword', function () {
 Route::get('/grades', function () {
     return Inertia::render('Student_Grades/StudentGrades');
 });
+
+// Route for Accounting
+Route::get('/accounting', function () {
+    return Inertia::render('Accounting/AccountingPage');
+});
+
+// Route for Viewing Payment History of Student
+Route::get('/accounting/paymenthistory/{studentId}', function ($studentId) {
+    return Inertia::render('Accounting/StaffAccounting/PaymentHistoryPage', [
+        'studentId' => $studentId,
+    ]);
+})->name('accounting.student.view');
+
+// Route for Viewing Payment Information
+Route::get('/accounting/paymentinfo/{paymentId}/{studentId}', function ($paymentId, $studentId) {
+    return Inertia::render('Accounting/StaffAccounting/PaymentInfo', [
+        'paymentId' => $paymentId,
+        'studentId' => $studentId,
+    ]);
+})->name('accounting.payment.view');
+
+
+
+
+
