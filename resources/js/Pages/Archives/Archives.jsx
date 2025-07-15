4e9e8e60667029cc7e6fc324216f61ee75df5608
Christian Anoyo <xtianseventeen@gmail.com>
2025-07-15 11:44:54 +0800

    feat: Add Archives page - Created tabs for archived courses, students, and staff

--- resources/js/Pages/Archives/Archives.jsx
+++ resources/js/Pages/Archives/Archives.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import Tabs from "../../Components/Tabs/Tabs";
+import ArchivedCourses from "./ArchiveComponents/ArchivedCourses";
+import ArchivedStudents from "./ArchiveComponents/ArchivedStudents";
+import ArchivedStaff from "./ArchiveComponents/ArchivedStaff";
 
 export default function Archives() {
-    return <div>ARCHIVES PAGE</div>;
+    const [activeTab, setActiveTab] = useState(0);
+    // Tab list
+    const tabs = ["Courses", "Students", "Administration"];
+    return (
+        <div className="space-y-5">
+            <Tabs
+                activeTab={activeTab}
+                doSomething={setActiveTab}
+                tabList={tabs}
+            />
+
+            {activeTab === 0 && <ArchivedCourses />}
+            {activeTab === 1 && <ArchivedStudents />}
+            {activeTab === 2 && <ArchivedStaff />}
+        </div>
+    );
 }
