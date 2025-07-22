8d357b991e2ffd5cb797dd368aa262d6c24d5c43
Christian Anoyo <xtianseventeen@gmail.com>
2025-07-22 15:06:07 +0800

    feat: Protect routes for password resets

--- routes/auth.php
+++ routes/auth.php
@@ -11,11 +11,17 @@ use Inertia\Inertia;
 Route::middleware('guest')->group(function() {
     Route::get('/login', [LoginController::class, 'show'])->middleware('preventBack')->name('login');
     Route::post('/login', [LoginController::class, 'loginUser'])->name('login.user');
+
     // Registration routes 
     // Render registration page
     Route::get('/register', [RegistrationController::class, 'show'])->middleware('preventBack')->name('register');
     // Register user
     Route::post('/register', [RegistrationController::class, 'registerUser'])->name('register.user');
+
+    Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+    Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post'); 
+    Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+    Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 });
 
 
@@ -28,10 +34,7 @@ Route::middleware('auth')->group(function() {
 });
 
 
-Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
-Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post'); 
-Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
-Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+
 
 
 
