1c01f954b0948b8c0393039d163808820443a1cb
Christian Anoyo <xtianseventeen@gmail.com>
2025-08-13 14:42:29 +0800

    feat: Make some columns in users table nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,16 +20,16 @@ return new class extends Migration
 
             $table->longText('profile_image')->nullable();
 
-            $table->string('birthdate'); 
-            $table->string('gender');
-            $table->string('contact_number');
+            $table->string('birthdate')->nullable(); 
+            $table->string('gender')->nullable();
+            $table->string('contact_number')->nullable();
 
             $table->string('email')->unique();
 
-            $table->string('house_no');
-            $table->string('province');
-            $table->string('city');
-            $table->string('barangay');
+            $table->string('house_no')->nullable();
+            $table->string('province')->nullable();
+            $table->string('city')->nullable();
+            $table->string('barangay')->nullable();
 
             $table->string('password');
 
