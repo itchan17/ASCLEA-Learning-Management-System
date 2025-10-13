63114a47017cdcd3697f6cbf70f31f624a375ea5
Mark Cueva <markcueva6@gmail.com>
2025-10-13 16:34:07 +0800

    Update profile page and routes

--- routes/profile.php
+++ routes/profile.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Auth\ProfileController;
 
 // Route for viewing profile page
-Route::get('/profile', function () {
-    return Inertia::render('Profile/Profile');
-})->middleware(['auth', 'verified', 'preventBack', 'checkRole:admin,faculty,student'])->name('profile');
+Route::middleware(['auth', 'verified', 'preventBack', 'checkRole:admin,faculty,student'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/photo', [ProfileController::class, 'updateProfile'])->name('profile.photo.update');
+
+});
 
