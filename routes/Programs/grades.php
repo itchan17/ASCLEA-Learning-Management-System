54dcc3cb14999c99d52bdca046caf9d039d0701a
Christian Anoyo <xtianseventeen@gmail.com>
2025-11-09 10:11:28 +0800

    feat: Add authorization in grade routes

--- routes/Programs/grades.php
+++ routes/Programs/grades.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Programs\GradeController;
+use App\Models\Programs\Grade;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('programs/{program}/courses/{course}')
@@ -8,14 +9,14 @@ Route::prefix('programs/{program}/courses/{course}')
     ->group(function () {
 
         // Route for grading the student
-        Route::post('/students/{assignedCourse}/grades', [GradeController::class, 'gradeStudent'])->name('student.grade');
+        Route::post('/students/{assignedCourse}/grades', [GradeController::class, 'gradeStudent'])->can('assignGrade', [Grade::class, 'course'])->name('student.grade');
 
         // Route for returning the student grades
-        Route::put('/students/grades', [GradeController::class, 'returnStudentGrade'])->name('return.student.grades');
+        Route::put('/students/grades', [GradeController::class, 'returnStudentGrade'])->can('returnGrades', [Grade::class, 'course'])->name('return.student.grades');
 
         // Route for downloading student grades pdf
-        Route::get('/students/grades/pdf', [GradeController::class, 'exportStudentGradesToPdf'])->name('export.student.grades.pdf');
+        Route::get('/students/grades/pdf', [GradeController::class, 'exportStudentGradesToPdf'])->can('downloadGrades', [Grade::class, 'course'])->name('export.student.grades.pdf');
 
         // Route for downloading student grades csv
-        Route::get('/students/grades/csv', [GradeController::class, 'exportActivityResponsesToCsv'])->name('export.student.grades.csv');
+        Route::get('/students/grades/csv', [GradeController::class, 'exportActivityResponsesToCsv'])->can('downloadGrades', [Grade::class, 'course'])->name('export.student.grades.csv');
     });
