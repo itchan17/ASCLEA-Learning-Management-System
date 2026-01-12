a1a03398dee3de6b9baa079d14bbf8c8ebb7ec2a
Christian Anoyo <xtianseventeen@gmail.com>
2026-01-12 15:57:18 +0800

    fix: Remove perrmanently deleting for post, material, section, and assessment, update datet display in each item
    - Remove commands that handle permanenlty delete of posts, materials, sections, and assessments
    
    - Update the date shown in the items based on its status
    
    - Remove the commands in the schedule

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::daily()
     ->group(function () {
-        Schedule::command('app:permanently-delete-assessments');
-        Schedule::command('app:permanently-delete-materials');
-        Schedule::command('app:permanently-delete-sections');
-        Schedule::command('app:permanently-delete-posts');
         Schedule::command('app:delete-old-rejected-students');
         Schedule::command('app:create-backup');
     });
